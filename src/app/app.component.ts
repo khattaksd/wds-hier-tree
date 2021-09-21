5afb93205c41d6ffa904e1606f0ec30469327466
salmankhattak <salman.khattak@wisetail.com>
2021-09-21 17:33:22 -0400

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,11 +73,13 @@ export class AppComponent implements OnInit {
     if (this.genTree.download) {
       if (this.genTree.csv) {
         let csvlines = 'code,value,parent\r\n';
-        nodes.forEach((n) => {
+        //skip ROOT node by starting at index 1
+        for (let index = 1; index < nodes.length; index++) {
+          const n = nodes[index];
           csvlines += n.value + ',' + n.code;
           if (n.parent) csvlines += ',' + n.parent;
           csvlines += '\r\n';
-        });
+        }
         blob = new Blob([csvlines], { type: 'data:text/csv;charset=utf-8' });
         filename = `genearated_${this.generatedTS}.csv`;
       } else {
